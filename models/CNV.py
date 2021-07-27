d4886fa6e8a98490a78fcc52ce91cb2f4164a823
Javier M. Duarte <jduarte@ucsd.edu>
2021-07-27 09:42:21 -0700

    add config parameters

--- models/CNV.py
+++ models/CNV.py
@@ -29,17 +29,10 @@ from .tensor_norm import TensorNorm
 from .common import CommonWeightQuant, CommonActQuant
 
 
-CNV_OUT_CH_POOL = [(64, False), (64, True), (128, False), (128, True), (256, False), (256, False)]
-INTERMEDIATE_FC_FEATURES = [(256, 512), (512, 512)]
-LAST_FC_IN_FEATURES = 512
-LAST_FC_PER_OUT_CH_SCALING = False
-POOL_SIZE = 2
-KERNEL_SIZE = 3
-
-
 class CNV(Module):
 
-    def __init__(self, num_classes, weight_bit_width, act_bit_width, in_bit_width, in_ch):
+    def __init__(self, num_classes, weight_bit_width, act_bit_width, in_bit_width, in_ch,
+                 cnv_out_ch_pool, int_fc_feat, last_fc_in_feat, pool_size, kern_size):
         super(CNV, self).__init__()
 
         self.conv_features = ModuleList()
@@ -53,9 +46,9 @@ class CNV(Module):
             narrow_range=False,
             restrict_scaling_type=RestrictValueType.POWER_OF_TWO))
 
-        for out_ch, is_pool_enabled in CNV_OUT_CH_POOL:
+        for out_ch, is_pool_enabled in cnv_out_ch_pool:
             self.conv_features.append(QuantConv2d(
-                kernel_size=KERNEL_SIZE,
+                kernel_size=kern_size,
                 in_channels=in_ch,
                 out_channels=out_ch,
                 bias=False,
@@ -67,9 +60,9 @@ class CNV(Module):
                 act_quant=CommonActQuant,
                 bit_width=act_bit_width))
             if is_pool_enabled:
-                self.conv_features.append(MaxPool2d(kernel_size=2))
+                self.conv_features.append(MaxPool2d(kernel_size=pool_size))
 
-        for in_features, out_features in INTERMEDIATE_FC_FEATURES:
+        for in_features, out_features in int_fc_feat:
             self.linear_features.append(QuantLinear(
                 in_features=in_features,
                 out_features=out_features,
@@ -82,7 +75,7 @@ class CNV(Module):
                 bit_width=act_bit_width))
 
         self.linear_features.append(QuantLinear(
-            in_features=LAST_FC_IN_FEATURES,
+            in_features=last_fc_in_feat,
             out_features=num_classes,
             bias=False,
             weight_quant=CommonWeightQuant,
@@ -118,10 +111,23 @@ def cnv(cfg):
     in_bit_width = cfg.getint('QUANT', 'IN_BIT_WIDTH')
     num_classes = cfg.getint('MODEL', 'NUM_CLASSES')
     in_channels = cfg.getint('MODEL', 'IN_CHANNELS')
+    cnv_out_ch_pool = cfg.get('MODEL', 'CNV_OUT_CH_POOL')
+    int_fc_feat = cfg.get('MODEL', 'INTERMEDIATE_FC_FEATURES')
+    cnv_out_ch_pool = [eval(pair) for pair in cnv_out_ch_pool.split('\n')]
+    int_fc_feat = [eval(pair) for pair in int_fc_feat.split('\n')]
+    last_fc_in_feat = cfg.getint('MODEL', 'LAST_FC_IN_FEATURES')
+    pool_size = cfg.getint('MODEL', 'POOL_SIZE')
+    kern_size = cfg.getint('MODEL', 'KERNEL_SIZE')
+
     net = CNV(weight_bit_width=weight_bit_width,
               act_bit_width=act_bit_width,
               in_bit_width=in_bit_width,
               num_classes=num_classes,
-              in_ch=in_channels)
+              in_ch=in_channels,
+              cnv_out_ch_pool=cnv_out_ch_pool,
+              int_fc_feat=int_fc_feat,
+              last_fc_in_feat=last_fc_in_feat,
+	      pool_size=pool_size,
+              kern_size=kern_size)
     return net
 
