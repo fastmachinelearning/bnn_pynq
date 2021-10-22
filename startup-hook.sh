a78d2b5167bf16b6deaa9e782bd1878c0c8a44f8
Jason Liang <74435747+jaysunl@users.noreply.github.com>
2021-10-21 20:05:33 -0700

    Update startup-hook.sh

--- startup-hook.sh
+++ startup-hook.sh
@@ -1,1 +1,6 @@
+# Install Brevitas
 python3 -m pip install brevitas
+
+# Install finn-base, with inference cost estimation
+pip install onnxoptimizer==0.2.6
+pip install git+https://github.com/Xilinx/finn-base.git@feature/itu_competition_21#egg=finn-base[onnx]
