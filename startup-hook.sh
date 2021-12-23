c7803084c089dd07a6698f1dbc964d948ef2ea55
Javier Duarte <jduarte@ucsd.edu>
2021-12-23 09:10:16 -0800

    fix model cost (#9)

--- startup-hook.sh
+++ startup-hook.sh
@@ -1,5 +1,5 @@
 # Install Brevitas
-python3 -m pip install brevitas
+python3 -m pip install brevitas==0.6.0
 # Install finn-base, with inference cost estimation
 python3 -m pip install onnxoptimizer==0.2.6
 python3 -m pip install git+https://github.com/Xilinx/finn-base.git@feature/itu_competition_21#egg=finn-base[onnx]
