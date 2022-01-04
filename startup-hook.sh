e4245e4bb555e5f9b2a28e1b2fd6638881782036
Javier M. Duarte <jduarte@ucsd.edu>
2022-01-03 19:33:36 -0800

    update docker image; run adaptive for bitwidth=1,2

--- startup-hook.sh
+++ startup-hook.sh
@@ -1,5 +1,2 @@
-# Install Brevitas
-python3 -m pip install brevitas==0.6.0
-# Install finn-base, with inference cost estimation
-python3 -m pip install onnxoptimizer==0.2.6
-python3 -m pip install git+https://github.com/Xilinx/finn-base.git@feature/itu_competition_21#egg=finn-base[onnx]
+# Install Brevitas, onnxoptimizer, finn-base, with inference cost estimation
+python3 -m pip install brevitas==0.6.0 onnxoptimizer==0.2.6 git+https://github.com/Xilinx/finn-base.git@feature/itu_competition_21#egg=finn-base[onnx]
