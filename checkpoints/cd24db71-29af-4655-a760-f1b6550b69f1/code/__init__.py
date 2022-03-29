c0fef9c49ee925ceca46e1d489024ccdeb0d6a8a
Tai <tan005@ucsd.edu>
2022-03-28 23:39:23 -0700

    deleted checkpoint

--- checkpoints/cd24db71-29af-4655-a760-f1b6550b69f1/code/__init__.py
+++
@@ -1,6 +0,0 @@
-# Fix mnist download on older torchvision versions
-from urllib import request
-
-opener = request.build_opener()
-opener.addheaders = [("User-agent", "Mozilla/5.0")]
-request.install_opener(opener)
