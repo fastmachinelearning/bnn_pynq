42687d9c2ea9d6ec21dd0a8cdbf4879e8d481d7a
Tai <tan005@ucsd.edu>
2022-03-28 23:44:05 -0700

    created baseline to recieve metric

--- recieve_graph.py
+++ recieve_graph.py
@@ -1,9 +1,12 @@
 from determined.experimental import client
 errs = 0 
 trial_list =  client.get_experiment(37).get_trials()
+
 for t in trial_list:
     try:
+       
         checks = t.top_checkpoint()
-        print(checks)
+        print(checks.validation['metrics']['validationMetrics']['bops'])
     except:
         errs = errs + 1 
+print(errs)
